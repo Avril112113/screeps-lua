4a28ac8008cac741c56264b6525b204edeadcc28
Dude112113 <curtisdude12@gmail.com>
2022-08-15 05:26:10 +0100

    Fixed timeouts with `-O2`

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ SET SCRIPT_DIR="C:\Users\dude\AppData\Local\Screeps\scripts\127_0_0_1___21025\de
 if exist "dist" rmdir "dist"
 mkdir "dist"
 
-cmd /C emcc --bind -s WASM=1 -s WASM_ASYNC_COMPILATION=0 -s MODULARIZE -s EXPORTED_FUNCTIONS=_init,_loop,_eval,_callWrappedLuaFunction -sEXPORTED_RUNTIME_METHODS=cwrap -I../lua-5.4.4/src -O1 --no-entry cpp/main.cpp lua.a -o dist/lua.js --embed-file src@/ --js-library js/jslib.js
+cmd /C emcc --bind -s WASM=1 -s WASM_ASYNC_COMPILATION=0 -s MODULARIZE -s EXPORTED_FUNCTIONS=_init,_loop,_eval,_callWrappedLuaFunction -sEXPORTED_RUNTIME_METHODS=cwrap -I../lua-5.4.4/src -O2 --no-entry cpp/main.cpp lua.a -o dist/lua.js --embed-file src@/ --js-library js/jslib.js
 
 copy /Y "js\main.js" "dist\main.js"
 move "dist\lua.js" "dist\lua_module.js"
