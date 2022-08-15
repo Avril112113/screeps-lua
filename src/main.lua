68d14a4ca3ca6e07b7edf5ab3cb6a5123050a0a6
Dude112113 <curtisdude12@gmail.com>
2022-08-15 14:04:56 +0100

    Lua: Added SPAWN_MAX_CREEPS

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,7 @@
+local SPAWN_MAX_BODY_COUNT = 12
+local SPAWN_MAX_CREEPS = 12
+
+
 local Tasking = require "tasking"
 
 
@@ -13,44 +17,46 @@ end
 function Script.loop()
 	for spawnName, spawn in pairs(Game.spawns) do
 		local creepCount = #spawn.room:find(102)
-		local body
-		local maxCost = creepCount < 4 and spawn.room.energyAvailable or spawn.room.energyCapacityAvailable
-		local workCount = 1
-		local carryCount = 1
-		local moveCount = 1
-		body = {WORK, CARRY, MOVE}
-		local cost = bodyCost(body)
-		while true do
-			local part
-			if carryCount/2 >= moveCount then
-				part = MOVE
-				moveCount = moveCount + 1
-			elseif workCount > carryCount or cost + Global.BODYPART_COST[WORK] > maxCost then
-				part = CARRY
-				carryCount = carryCount + 1
-			else
-				part = WORK
-				workCount = workCount + 1
+		if creepCount < SPAWN_MAX_CREEPS then
+			local body
+			local maxCost = creepCount < 4 and spawn.room.energyAvailable or spawn.room.energyCapacityAvailable
+			local workCount = 1
+			local carryCount = 1
+			local moveCount = 1
+			body = {WORK, CARRY, MOVE}
+			local cost = bodyCost(body)
+			while true do
+				local part
+				if carryCount/2 >= moveCount then
+					part = MOVE
+					moveCount = moveCount + 1
+				elseif workCount > carryCount or cost + Global.BODYPART_COST[WORK] > maxCost then
+					part = CARRY
+					carryCount = carryCount + 1
+				else
+					part = WORK
+					workCount = workCount + 1
+				end
+				local partCost = Global.BODYPART_COST[part]
+				if cost + partCost > maxCost then
+					break
+				end
+				table.insert(body, part)
+				cost = cost + partCost
+				if #body >= Global.MAX_CREEP_SIZE or #body >= SPAWN_MAX_BODY_COUNT then
+					break
+				end
 			end
-			local partCost = Global.BODYPART_COST[part]
-			if cost + partCost > maxCost then
-				break
+			table.sort(body)
+			local result = spawn:spawnCreep(body, ("Creep %.0f"):format(Game.time))
+			if result ~= 0 and result ~= -6 and result ~= -4 then
+				print_error("SPAWN " .. spawnName .. " ERROR: " .. result)
+			elseif result == 0 then
+				print_info("Spawned  creep that cost " .. bodyCost(body) .. "/" .. maxCost .. "/" .. spawn.room.energyCapacityAvailable .. " with [" .. table.concat(body, ", ") .. "]")
 			end
-			table.insert(body, part)
-			cost = cost + partCost
-			if #body >= Global.MAX_CREEP_SIZE then
-				break
+			if result ~= 0 and spawn.room.energyCapacityAvailable == spawn.room.energyAvailable then
+				print_warn("At capacity of " .. spawn.room.energyCapacityAvailable .. " but no creep that cost " .. bodyCost(body) .. " with [" .. table.concat(body, ", ") .. "] spawned?")
 			end
-		end
-		table.sort(body)
-		local result = spawn:spawnCreep(body, ("Creep %.0f"):format(Game.time))
-		if result ~= 0 and result ~= -6 and result ~= -4 then
-			print_error("SPAWN " .. spawnName .. " ERROR: " .. result)
-		elseif result == 0 then
-			print_info("Spawned  creep that cost " .. bodyCost(body) .. "/" .. maxCost .. "/" .. spawn.room.energyCapacityAvailable .. " with [" .. table.concat(body, ", ") .. "]")
-		end
-		if result ~= 0 and spawn.room.energyCapacityAvailable == spawn.room.energyAvailable then
-			print_warn("At capacity of " .. spawn.room.energyCapacityAvailable .. " but no creep that cost " .. bodyCost(body) .. " with [" .. table.concat(body, ", ") .. "] spawned?")
 		end
 		spawn.room.visual:text(tostring(creepCount), spawn.pos)
 	end
